b6011e3caecfa7367a9f07aeb7a874c36b2d180c
Johannes Russek <johannes.russek@io-consulting.net>
2010-05-07 19:13:21 +0200

    provide Service with hooks to add user definied runurl scripts

--- lang/java/common/src/main/java/org/apache/whirr/service/Service.java
+++ lang/java/common/src/main/java/org/apache/whirr/service/Service.java
@@ -2,15 +2,73 @@ package org.apache.whirr.service;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 import org.jclouds.compute.ComputeService;
 
 public abstract class Service {
+
   protected ServiceSpec serviceSpec;
+  
+  private final Hashtable<String, List<RunUrlScript>> runUrlScripts = new Hashtable();
 
   public Service(ServiceSpec serviceSpec) {
     this.serviceSpec = serviceSpec;
   }
-  
+
+  /**
+   * add a list of runurl scripts to be run by the userdata on the nodes at a point
+   * identified by label
+   * @param label   the hook-label this script should be run by
+   * @param scripts List of RunUrlScript Objects to run
+   */
+  public void addScript(String label, List<RunUrlScript> scripts) {
+      if(runUrlScripts.get(label) != null) {
+          runUrlScripts.get(label).addAll(scripts);
+      }
+      runUrlScripts.put(label, scripts);
+  }
+
+  /**
+   * add a list of runurl scripts to be run by the userdata on the nodes after
+   * all standard runurl scripts have been run
+   * @param scripts List of RunUrlScript Objects to run
+   */
+  public void addScript(List<RunUrlScript> scripts) {
+     addScript("DEFAULT",scripts);
+  }
+
+  /**
+   * add a runurl script to be run by the userdata on the nodes at a point
+   * identified by label
+   * @param label
+   * @param script
+   */
+  public void addScript(String label, RunUrlScript script) {
+      ArrayList<RunUrlScript> scripts = new ArrayList<RunUrlScript>();
+      scripts.add(script);
+      addScript(label,scripts);
+  }
+
+  /**
+   * add a runurl script to be run by the userdata on the nodes after all standard
+   * runurl scripts have been run
+   * @param script
+   */
+  public void addScript(RunUrlScript script) {
+      addScript("DEFAULT",script);
+  }
+
+  /**
+   * retrieve a list of RunUrlScript Objects identified by label
+   * @param label   the label of the List
+   * @return    returns a List<RunUrlscript> containing the Objects
+   */
+  public List<RunUrlScript> getByLabel(String label) {
+     return new ArrayList<RunUrlScript>(runUrlScripts.get(label));
+  }
+
   public abstract Cluster launchCluster(ClusterSpec clusterSpec)
     throws IOException;
   
